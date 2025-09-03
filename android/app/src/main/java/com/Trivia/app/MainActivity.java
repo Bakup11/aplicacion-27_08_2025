09f958892be330961140e5ec75e2186f0d663897
Wilson Andres Carmona Barco <wilsonandres08@hotmail.com>
2025-09-03 14:08:25 +0000

    re configuracion desde 0

--- android/app/src/main/java/com/Trivia/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.Trivia.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ proyecto/android/app/src/main/java/com/trivia/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.trivia.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
